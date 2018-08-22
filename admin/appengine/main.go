f85a17e17337d35f67f81be91e995053a7e3f5d4
akm <akm2000@gmail.com>
2018-08-22 19:57:00 +0900

    :+1: Follow the instruction by make main
    1. Change package from "main" to "appengine"
    2. Add "net/http" to import section
    3. Add "github.com/akm/gae_go-datastore-goa-goon-viron-react-redux-example/admin/controller" to import section
    4. Change "func main()" to "func init()"
    5. Add "controller." before each "NewXxxxController"
    6. Comment out the lines below the comment "Start service"
    7. Add http.HandleFunc("/", service.Mux.ServeHTTP) at the end of init func

--- admin/appengine/main.go
+++ admin/appengine/main.go
@@ -1,14 +1,17 @@
 //go:generate goagen bootstrap -d github.com/akm/gae_go-datastore-goa-goon-viron-react-redux-example/admin/design
 
-package main
+package appengine
 
 import (
+	"net/http"
+
 	"github.com/akm/gae_go-datastore-goa-goon-viron-react-redux-example/admin/app"
+	"github.com/akm/gae_go-datastore-goa-goon-viron-react-redux-example/admin/controller"
 	"github.com/goadesign/goa"
 	"github.com/goadesign/goa/middleware"
 )
 
-func main() {
+func init() {
 	// Create service
 	service := goa.New("appengine")
 
@@ -19,18 +22,19 @@ func main() {
 	service.Use(middleware.Recover())
 
 	// Mount "memos" controller
-	c := NewMemosController(service)
+	c := controller.NewMemosController(service)
 	app.MountMemosController(service, c)
 	// Mount "swagger" controller
-	c2 := NewSwaggerController(service)
+	c2 := controller.NewSwaggerController(service)
 	app.MountSwaggerController(service, c2)
 	// Mount "users" controller
-	c3 := NewUsersController(service)
+	c3 := controller.NewUsersController(service)
 	app.MountUsersController(service, c3)
 
 	// Start service
-	if err := service.ListenAndServe(":8080"); err != nil {
-		service.LogError("startup", "err", err)
-	}
+	// if err := service.ListenAndServe(":8080"); err != nil {
+	// 	service.LogError("startup", "err", err)
+	// }
 
+	http.HandleFunc("/", service.Mux.ServeHTTP)
 }
