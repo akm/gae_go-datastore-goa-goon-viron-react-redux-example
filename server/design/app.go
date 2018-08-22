4f230768fb06886a208ae6145d9adc55fb1c38ac
akm <akm2000@gmail.com>
2018-08-22 16:04:53 +0900

    :+1: Use environment variables for Host and Scheme in desgin/app.go

--- server/design/app.go
+++ server/design/app.go
@@ -1,17 +1,29 @@
 package design
 
 import (
+	"os"
+
 	. "github.com/goadesign/goa/design"
 	. "github.com/goadesign/goa/design/apidsl"
 )
 
 const DefaultResponseTrait = "DefaultResponseTrait"
 
+func env(key, defaultValue string) string {
+	r := os.Getenv(key)
+	if r == "" {
+		return defaultValue
+	}
+	return r
+}
+
 var _ = API("appengine", func() {
 	Title("The appengine example")
 	Description("A simple appengine example")
-	Host("localhost:8080")
-	Scheme("http")
+
+	Host(env("GOA_HOST", "localhost:8081"))
+	Scheme(env("GOA_SCHEME", "http"))
+
 	BasePath("/")
 	Origin("*", func() {
 		Methods("GET", "POST", "PUT", "DELETE", "OPTIONS")
