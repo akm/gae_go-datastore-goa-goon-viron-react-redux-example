f232eec9a4ea2c3e691e4aeec72b6c5d3d1da8a6
akm <akm2000@gmail.com>
2018-08-28 14:45:06 +0900

    :up: Add config for rollup-plugin-serve

--- ui/rollup.config.js
+++ ui/rollup.config.js
@@ -1,6 +1,7 @@
 // node-resolve will resolve all the node dependencies
 
 import babel from 'rollup-plugin-babel'
+import serve from 'rollup-plugin-serve'
 
 export default {
   input: 'src/index.js',
@@ -11,6 +12,11 @@ export default {
   plugins: [
     babel({
       exclude: 'node_modules/**'
+    }),
+    // https://github.com/thgh/rollup-plugin-serve#options
+    serve({
+      contentBase: 'dist',
+      port: 8080
     })
   ]
 };
