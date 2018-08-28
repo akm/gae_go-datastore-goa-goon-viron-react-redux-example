41f107f64c202b2a4172e279ca764acdfa16ae7d
akm <akm2000@gmail.com>
2018-08-28 14:33:03 +0900

    :up: Add config for rollup-plugin-babel

--- ui/rollup.config.js
+++ ui/rollup.config.js
@@ -1,7 +1,19 @@
+// node-resolve will resolve all the node dependencies
+// import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+
 export default {
   input: 'src/index.js',
   output: {
     file: 'dist/bundle.js',
     format: 'cjs'
-  }
+  },
+  external: [
+    'react'
+  ],
+  plugins: [
+    babel({
+      exclude: 'node_modules/**'
+    })
+  ]
 };
