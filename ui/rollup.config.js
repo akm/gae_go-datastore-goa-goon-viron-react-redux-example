6e93ac530b135c2f490c71cc85fca8f8e71f8151
akm <akm2000@gmail.com>
2018-08-28 17:04:47 +0900

    :up: Add config for rollup-plugin-commonjs
    See https://github.com/rollup/rollup-plugin-commonjs/issues/200#issuecomment-323617247

--- ui/rollup.config.js
+++ ui/rollup.config.js
@@ -1,5 +1,6 @@
 // node-resolve will resolve all the node dependencies
 import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
 import serve from 'rollup-plugin-serve'
 
@@ -15,10 +16,22 @@ export default {
     'react-proptypes'
   ],
   plugins: [
-    resolve(),
     babel({
       exclude: 'node_modules/**'
     }),
+    commonjs({
+      exclude: 'node_modules/process-es6/**',
+      include: [
+        //some react related modules i need
+        'node_modules/fbjs/**',
+        'node_modules/object-assign/**',
+        'node_modules/react/**',
+        'node_modules/react-dom/**',
+        'node_modules/prop-types/**',
+        'node_modules/create-react-class/**' // adding the module with that "default not exported by" message to this includes list, made that message go away
+      ]
+    }),
+    resolve(),
     // https://github.com/thgh/rollup-plugin-serve#options
     serve({
       contentBase: 'dist',
