b6bcf33919d18099d07a9c134ed51c2405ba774d
akm <akm2000@gmail.com>
2018-08-28 17:28:10 +0900

    :up: Add config for node-resolve and commonjs

--- ui/rollup.config.js
+++ ui/rollup.config.js
@@ -1,5 +1,6 @@
 // node-resolve will resolve all the node dependencies
-// import resolve from 'rollup-plugin-node-resolve';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import serve from 'rollup-plugin-serve'
 
@@ -7,15 +8,28 @@ export default {
   input: 'src/index.js',
   output: {
     file: 'dist/bundle.js',
-    format: 'cjs'
+    format: 'iife'
   },
-  external: [
-    'react'
-  ],
+  // external: [
+  //   'react'
+  // ],
   plugins: [
     babel({
       exclude: 'node_modules/**'
     }),
+    commonjs({
+      exclude: 'node_modules/process-es6/**',
+      include: [
+        //some react related modules i need
+        'node_modules/fbjs/**',
+        'node_modules/object-assign/**',
+        'node_modules/react/**',
+        'node_modules/react-dom/**',
+        'node_modules/prop-types/**',
+        'node_modules/create-react-class/**' // adding the module with that "default not exported by" message to this includes list, made that message go away
+      ]
+    }),
+    resolve(),
     serve('dist')
   ]
 };
