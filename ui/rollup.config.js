740a00cb29c10b5d0a041575038d8cbe46cce23c
akm <akm2000@gmail.com>
2018-08-28 17:33:03 +0900

    :up: Use rollup-plugin-replace to replace NODE_ENV

--- ui/rollup.config.js
+++ ui/rollup.config.js
@@ -2,6 +2,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
 import serve from 'rollup-plugin-serve'
 
 export default {
@@ -30,6 +31,9 @@ export default {
       ]
     }),
     resolve(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify( 'production' )
+    }),
     serve('dist')
   ]
 };
