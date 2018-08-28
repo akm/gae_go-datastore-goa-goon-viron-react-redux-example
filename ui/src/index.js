af6a3995c1c8a7a27f932a7ea9e6d0c8422cd026
akm <akm2000@gmail.com>
2018-08-28 14:25:43 +0900

    :+1: Add initial source code

---
+++ ui/src/index.js
@@ -0,0 +1,9 @@
+// src/App.js
+import React from 'react';
+import ReactDom from 'react-dom';
+import Root from './components/Root';
+
+ReactDom.render(
+    <Root />,
+    document.getElementById('root'),
+);
