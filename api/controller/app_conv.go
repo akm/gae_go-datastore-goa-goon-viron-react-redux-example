bda0032e624babdbb855b249ba8e3adff357da35
akm <akm2000@gmail.com>
2018-08-29 17:35:02 +0900

    :factory: make generate

--- api/controller/app_conv.go
+++ api/controller/app_conv.go
@@ -5,32 +5,43 @@ import (
 	"github.com/akm/gae_go-datastore-goa-goon-viron-react-redux-example/api/model"
 )
 
-func MemoPayloadToModel(src *app.MemoPayload) model.Memo {
-	if src == nil {
-		return model.Memo{}
+func MemoPayloadToModel(payload *app.MemoPayload) (*model.Memo, error) {
+	model := &model.Memo{}
+	if err := CopyFromMemoPayloadToModel(payload, model); err != nil {
+		return nil, err
 	}
-	return model.Memo{
-		Content: src.Content,
-		Shared:  BoolPointerToBool(src.Shared),
-		// Id no payload field
-		// AuthorKey no payload field
-		// CreatedAt no payload field
-		// UpdatedAt no payload field
-		// No model field for payload field "created_by"
-	}
+	return model, nil
 }
 
-func MemoModelToMediaType(src *model.Memo) *app.Memo {
-	if src == nil {
-		return nil
+func CopyFromMemoPayloadToModel(payload *app.MemoPayload, model *model.Memo) error {
+	if payload == nil {
+		return NoPayloadGiven
 	}
-	return &app.Memo{
-		ID:        Int64ToString(src.Id),
-		Content:   src.Content,
-		Shared:    src.Shared,
-		CreatedAt: src.CreatedAt,
-		UpdatedAt: src.UpdatedAt,
-		// AuthorKey no media type field
-		// No field for media type field "id"
+	if model == nil {
+		return NoModelGiven
 	}
+
+	// Id not found in payload fields
+	// AuthorKey not found in payload fields
+	model.Content = payload.Content
+	model.Shared = BoolPointerToBool(payload.Shared)
+	// CreatedAt not found in payload fields
+	// UpdatedAt not found in payload fields
+	// No model field for payload field "created_by"
+	return nil
+}
+
+func MemoModelToMediaType(model *model.Memo) (*app.Memo, error) {
+	if model == nil {
+		return nil, NoModelGiven
+	}
+	r := &app.Memo{}
+
+	r.ID = Int64ToString(model.Id)
+	// AuthorKey not found for media type field
+	r.Content = model.Content
+	r.Shared = model.Shared
+	r.CreatedAt = model.CreatedAt
+	r.UpdatedAt = model.UpdatedAt
+	return r, nil
 }
