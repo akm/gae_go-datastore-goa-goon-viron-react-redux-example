abe16c9cc4e5a827c3baabee612e618da2527927
akm <akm2000@gmail.com>
2018-08-29 21:27:14 +0900

    :+1: Use client in Root component

--- ui/src/components/Root.js
+++ ui/src/components/Root.js
@@ -1,10 +1,18 @@
 // src/components/Root.js
 import React from 'react';
 
-const Root = () => (
+import client from '../api/client'
+
+const Root = () => {
+  client().listMemos("/memos")
+    .then( (resp) => console.log("SUCCESS response", resp.statusText) )
+    .catch( (resp) => console.log("ERROR response", resp.statusText) );
+
+  return (
     <div>
         Hello World
     </div>
-);
+  )
+};
 
 export default Root
