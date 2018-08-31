19f3eb76886d6ca59de224752da0ff638f775b73
akm <akm2000@gmail.com>
2018-08-31 21:37:36 +0900

    :+1: Pass memos as property to MemoList

--- ui/src/components/MemoList.js
+++ ui/src/components/MemoList.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 
-const MemoList = () => (
-  <ListGroup>
-    <ListGroupItem>Cras justo odio</ListGroupItem>
-    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-    <ListGroupItem>Morbi leo risus</ListGroupItem>
-    <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-    <ListGroupItem>Vestibulum at eros</ListGroupItem>
-  </ListGroup>
-)
+const MemoList = ({memos}) => {
+  console.log("MemoList memos", memos)
+  return (
+    <ListGroup>
+      { memos.map((memo) => {
+        return <ListGroupItem>{memo}</ListGroupItem>;
+      }) }
+    </ListGroup>
+  )
+}
 
 export default MemoList
