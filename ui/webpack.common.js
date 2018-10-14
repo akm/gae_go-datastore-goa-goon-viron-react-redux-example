17b0e531a680903ccb39a7c9ac0ede99cd7e82a5
hokuts <hokuto.sh@gmail.com>
2018-10-14 18:23:44 +0900

    add source map settings

--- ui/webpack.common.js
+++ ui/webpack.common.js
@@ -2,26 +2,57 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const path = require('path');
 
-module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".json"]
-  },
-  module: {
-    rules: [
-      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
-      { test: /\.hbs$/, loader: 'handlebars-loader' },
-      { test: /\.css/, use: [ 'style-loader', { loader: 'css-loader', options: {url: false} } ],},
-      { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
+module.exports = ({sourceMap=false} = {}) => { 
+  return {
+    entry: "./src/index.tsx",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist")
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+    module: {
+      rules: [
+        { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+        { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+        { test: /\.hbs$/, loader: 'handlebars-loader' },
+        {
+          test: /\.css/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                url: false,
+                sourceMap: sourceMap
+              }
+            }
+          ]
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            "style-loader",
+            {
+              loader: 'css-loader',
+              options: {
+                url: false,
+                sourceMap: sourceMap,
+                importLoaders: 2
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {sourceMap: sourceMap } 
+            }
+          ]
+        },
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({template: 'src/templates/index.hbs'}),
+      new CleanWebpackPlugin(["dist"]),
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({template: 'src/templates/index.hbs'}),
-    new CleanWebpackPlugin(["dist"]),
-  ]
+  }
 };
--- ui/webpack.dev.js
+++ ui/webpack.dev.js
@@ -1,7 +1,7 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-module.exports = merge(common, {
+module.exports = merge(common({sourceMap: true}), {
   mode: 'development',
   devtool: 'source-map',
   devServer: {
